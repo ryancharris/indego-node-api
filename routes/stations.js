6aa0694af719652df183063d8f6451694f19072a
Ryan Harris <ryan.harris@revzilla.com>
2017-11-03 21:57:06 -0400

    Add axios and update comments

--- routes/stations.js
+++ routes/stations.js
@@ -1,32 +1,41 @@
 var express = require('express');
 var router = express.Router();
 var _ = require('lodash');
+var axios = require('axios');
 
 router.get('/', function(req, res) {
-  var query = req.query;
+  // Parse query string
+  var at = req.query.at;
 
+  // Make request to Indego GeoJSON API
+  axios.get('https://www.rideindego.com/stations/json/')
+    .then(function(response) {
+      console.log(response.data);
+    })
+    .catch(function(errors) {
+      console.log(error);
+    });
+
+  // Send server response as JSON
   res.json({
-    query
+    at,
+    response
   });
 });
 
 router.get('/:kioskId', function(req, res) {
-  var query = req.query;
-
-  var queryStringArray = _.toPairs(query);
+  // Parse query string and create object from params
+  var queryStringArray = _.toPairs(req.query);
   var queryString = _.head(queryStringArray).join("=");
   var querySplit = _.split(queryString, ',');
-
   var arrays = querySplit.map(function(element) {
     return element.split('=');
   });
 
   var query = _.fromPairs(arrays);
-
-  console.log(query);
-
   var kioskId = req.params.kioskId;
 
+  // Send server response as JSON
   res.json({
     kioskId,
     query
