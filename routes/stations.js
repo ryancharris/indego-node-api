fb3de92b960f8205565642bd7cba3ee86c34f447
Ryan Harris <ryan.harris@revzilla.com>
2017-11-05 15:51:04 -0500

    Refactor functions and callbacks

--- routes/stations.js
+++ routes/stations.js
@@ -18,6 +18,8 @@ router.get('/', function(req, res) {
 
   // Make request to Indego GeoJSON API
   fetchStationData(returnStationData);
+
+  // Fetch weather data
   fetchWeatherData(returnWeatherData);
 
   // Send server response as JSON
@@ -30,26 +32,35 @@ router.get('/', function(req, res) {
 
 router.get('/:kioskId', function(req, res) {
   // Parse query string and create object from params
-  var queryStringArray = _.toPairs(req.query);
-  var queryString = _.head(queryStringArray).join("=");
-  var querySplit = _.split(queryString, ',');
-  var arrays = querySplit.map(function(element) {
-    return element.split('=');
-  });
+  var at = parseQueryParams(req.query);
 
-  var query = _.fromPairs(arrays);
+  // Parse query params
   var kioskId = req.params.kioskId;
+
+  // Fetch weather data
+  fetchWeatherData(returnWeatherData);
 
   // Send server response as JSON
   res.json({
-    kioskId,
-    query
+    at,
+    weather
   });
 });
 
 //
 // HELPER FUNCTIONS
 //
+
+function parseQueryParams(query) {
+  var queryStringArray = _.toPairs(query);
+  var queryString = _.head(queryStringArray).join("=");
+  var querySplit = _.split(queryString, ',');
+  var arrays = querySplit.map(function(element) {
+    return element.split('=');
+  });
+  var query = _.fromPairs(arrays);
+  return query.at;
+}
 
 function fetchStationData(callback) {
   request('https://www.rideindego.com/stations/json/', function(err, res, body) {
@@ -59,18 +70,18 @@ function fetchStationData(callback) {
 }
 
 function returnStationData() {
-  console.log(stations);
+  console.info(stations);
 }
 
 function fetchWeatherData(callback) {
-  request(`https://api.openweathermap.org/data/2.5/forecast?id=4560349&APPID=${WEATHER_API_KEY}`, function(err, res, body) {
+  request(`https://api.openweathermap.org/data/2.5/weather?id=4560349&APPID=${WEATHER_API_KEY}`, function(err, res, body) {
     weather = JSON.parse(body);
     callback();
   });
 }
 
 function returnWeatherData() {
-  console.log(weather);
+  console.info(weather);
 }
 
 module.exports = router;
